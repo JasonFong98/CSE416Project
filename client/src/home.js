22a421f146f5da712b96b37a5182a016c816d7c5
Jason Fong <jasonfong12398@gmail.com>
2022-09-20 22:19:59 -0400

    removed tool box

--- client/src/home.js
+++ client/src/home.js
@@ -1,17 +1,10 @@
 import React from "react";
-import AppBar from "react-toolbox/lib/app_bar";
-import Navigation from "react-toolbox/lib/navigation";
-import Link from "react-toolbox/lib/link";
+
 
 const Home = (props) => {
   return (
     <div>
-      <AppBar title="H.R. 3863" leftIcon="menu">
-        <Navigation type="horizontal">
-          <Link href="http://" label="Inbox" icon="inbox" />
-          <Link href="http://" active label="Profile" icon="person" />
-        </Navigation>
-      </AppBar>
+
     </div>
   );
 };
