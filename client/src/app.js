e9d4962786c75d306a703c36f18f1d14ff677d38
Jason Fong <jasonfong12398@gmail.com>
2022-09-21 16:11:04 -0400

    making state files

---
+++ client/src/app.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import States from './states/states';
+
+
+const App = (props) =>{
+    const [state, setState] = useState("home");
+
+    return ( <div>
+
+    </div> );
+}
+
+export default App;
---
+++ client/src/states/states.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+const States = () => {
+    return ( <div>
+
+    </div> );
+}
+
+export default States;
