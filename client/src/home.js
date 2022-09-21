d210e619b0465c41121e5692e0b969dd3e9290e2
Jason Fong <jasonfong12398@gmail.com>
2022-09-20 23:09:10 -0400

    change home size

--- client/src/home.js
+++ client/src/home.js
@@ -6,8 +6,8 @@ const Home = (props) => {
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Container fluid style={{paddingLeft: "3%"}}>
-          <Navbar.Brand href="/"><h1 style={{fontSize:30}}>Home</h1></Navbar.Brand>
+        <Container fluid style={{paddingLeft: "2%"}}>
+          <Navbar.Brand href="/"><h1 style={{fontSize:20}}>Home</h1></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
           </Navbar.Collapse>
