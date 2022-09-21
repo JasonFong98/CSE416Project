f80ab2ecdcfc8678ccd9c9c5e969d84dd97993db
Johnathan Zhao <johnathan.zhao@stonybrook.edu>
2022-09-21 11:38:43 -0400

    Added shit

--- client/src/home.js
+++ client/src/home.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { Container,Nav,Navbar,NavDropdown } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import USAMap from "react-usa-map";
-import './home.css';
+import {MapContainer,GeoJSON} from "react-leaflet";
+
+import USMap from "./geojson_data/tl_2021_us_state.json"
+
+import "./home.css";
 
 const Home = (props) => {
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Container fluid style={{paddingLeft: "2%"}}>
-          <Navbar.Brand href="/"><h1 style={{fontSize:20}}>Home</h1></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-          </Navbar.Collapse>
+        <Container fluid style={{ paddingLeft: "2%" }}>
+          <Navbar.Brand href="/">
+            <h1 style={{ fontSize: 20 }}>Home</h1>
+          </Navbar.Brand>
         </Container>
       </Navbar>
-
-      <USAMap/>
+      <h1 style={{ textAlign: "center" }}>H.R. 3863</h1>
+      <MapContainer style={{height:"60vh"}} center={[45,-85]} zoom={4}>
+        <GeoJSON data={USMap.features}/>
+      </MapContainer>
     </div>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Home from './home';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render(
-  <React.StrictMode>  
-      <Home/>
-  </React.StrictMode>,
+const root= ReactDOM.createRoot(
   document.getElementById('root')
 );
+
+root.render(<Home/>);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
