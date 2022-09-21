3972b66e87c8e976a9d865d2a228a539d13ccfb0
Johnathan Zhao <johnathan.zhao@stonybrook.edu>
2022-09-21 13:23:55 -0400

    Changed home screen

--- client/src/home.js
+++ client/src/home.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Container, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {MapContainer,GeoJSON} from "react-leaflet";
-import 'leaflet/dist/leaflet.css';
+import { MapContainer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 import USMap from "./geojson_data/us-states.json";
 
 import "./home.css";
 
 const Home = (props) => {
+  let state_style = {
+    weight: 0.8,
+    fillColor: "lightgreen",
+    color: "green",
+  };
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg">
@@ -18,10 +23,22 @@ const Home = (props) => {
           </Navbar.Brand>
         </Container>
       </Navbar>
-      <h1 style={{ textAlign: "center" }}>H.R. 3863</h1>
-      <div style={{width:"50%", height:"70%", position:"relative", left:"25%", backgroundColor: "white"}}>
-        <MapContainer style={{height:"60vh"}} center={[35,-90]} zoom={4} scrollWheelZoom={false}>
-            <GeoJSON data={USMap.features}/>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "relative",
+          backgroundColor: "white",
+        }}
+      >
+        <MapContainer
+          style={{ height: "93.5vh" }}
+          center={[35, -100]}
+          zoom={4}
+          scrollWheelZoom={false}
+        >
+          <h1 style={{ textAlign: "center" }}>H.R. 3863</h1>
+          <GeoJSON style={state_style} data={USMap.features} />
         </MapContainer>
       </div>
     </div>
