09a07064b1fd5ee066978b67b77e371dca43a7db
Johnathan Zhao <johnathan.zhao@stonybrook.edu>
2022-11-05 15:23:32 -0400

    Changed server and moved it to Intellij

--- server/server/src/main/java/com/example/server/ServerApplication.java
+++ server/server/src/main/java/com/example/server/ServerApplication.java
@@ -1,15 +1,10 @@
-package com.server.server;
+package com.example.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.server.configuration.DbConfiguration;
 
 @SpringBootApplication
-@EnableConfigurationProperties(DbConfiguration.class)
 public class ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
