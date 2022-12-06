6e9662bb64afe1f9dcdd04d33217f26f9debb61a
Cheng Yang <chengyang@Chengs-MacBook-Pro.local>
2022-12-06 13:29:51 -0500

    drop down menu except setbound

--- patch2.0/client/src/components/map/usmap.jsx
+++ patch2.0/client/src/components/map/usmap.jsx
@@ -1,7 +1,13 @@
+import * as React from 'react';
 import { useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { GeoJSON, useMap } from "react-leaflet";
 import api from "../../api/api";
+import MuiButton from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const USMap = () => {
   const states = ["Florida", "North Carolina", "Ohio"];
@@ -14,6 +20,20 @@ const USMap = () => {
     opacity: 0.5,
     // color: "#EDE4E0",
   };
+
+  const handleMenu = (stateCode, name, popupState) => {
+    api.getStateMap(stateCode).then((res) => {
+      geoJsonLayer.current.addData(
+        res.data.ensemble.currentDistrictPlan.features
+      );
+    });
+    document.getElementById('state-menu').style.display="none";
+    popupState.close();
+   //map.fitBounds(stateBounds.get(name));
+    console.log("JEASDASJDAJISDJIADIAISUHDIADHIUDIAUDHIUIUHIUIUHIHU")
+    console.log(map)
+    navigate(`/home/${name}`, { state: { stateCode } });
+  }
 
   const onEachFeature = (state, layer) => {
     if (states.includes(state.properties.NAME)) {
@@ -57,11 +77,29 @@ const USMap = () => {
   }, []);
 
   return (
-    <GeoJSON
+    <div>
+      <div id="state-menu">
+          <PopupState variant="popover" popupId="demo-popup-menu">
+              {(popupState) => (
+                <React.Fragment>
+                  <MuiButton variant="contained" {...bindTrigger(popupState)}>
+                    Select a state<ArrowDropDownIcon/>
+                </MuiButton>
+                  <Menu {...bindMenu(popupState)}>
+                    <MenuItem onClick={() => handleMenu('FL', 'Florida', popupState)}>Florida</MenuItem>
+                    <MenuItem onClick={() => handleMenu('NC', 'North Carolina', popupState)}>North Carolina</MenuItem>
+                    <MenuItem onClick={() => handleMenu('OH', 'Ohio', popupState)}>Ohio</MenuItem>
+                  </Menu>
+                </React.Fragment>
+              )}
+          </PopupState>
+      </div>
+      <GeoJSON
       ref={geoJsonLayerRef}
       style={stateStyle}
       onEachFeature={onEachFeature}
     />
+    </div>
   );
 };
 
